3d0fc2dfa2e29ca6687286ce2a9fead7f86f9d72
Hero <gzoo@uzwelo.com>
2023-07-18 12:45:54 -0400

    stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,12 @@
 <script lang="ts">
-    export let name: string;
+
+    import Test from "./components/Test/file.svelte";
 </script>
 
 <main>
-    <h1>Hello {name}!</h1>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <Test name = 5/>
 </main>
 
 <style lang="scss">
-    main {
-        text-align: center;
-        padding: 1em;
-        max-width: 240px;
-        margin: 0 auto;
-    }
 
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
 </style>
